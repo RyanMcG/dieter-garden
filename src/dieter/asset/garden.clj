89fcfee907aa727fda8366821aa74ecc7e6a80da
Ryan McGowan <ryan@ryanmcg.com>
2013-04-09 23:36:32 -0400

    Stub out asset.garden.

--- src/dieter/asset/garden.clj
+++ src/dieter/asset/garden.clj
@@ -1,6 +1,23 @@
-(ns dieter.asset.garden)
+(ns dieter.asset.garden
+  (:require [garden.core :refer [css]]
+            [dieter.pools :as pools]
+            [dieter.settings :as settings]
+            [dieter.asset :as asset]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn preprocess-garden*
+  "Actually preproccess the given garden file."
+  [file]
+  (css (load-file file)))
+
+(defn preprocess-garden
+  "Preprocess the given file as garden then pass it on."
+  [file]
+  (asset/memoize-file file preprocess-garden*))
+
+(defrecord Garden [file]
+  dieter.asset.Asset
+  (read-asset [this _]
+    (let [file (:file this)]
+      (assoc this :content (preprocess-garden file)))))
+
+(asset/register "garden" map->Garden)
